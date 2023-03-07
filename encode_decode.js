d44fed80c98325d7b1cff640cf7aa47e1708bb34
ronalatifaj <rona.latifaj@student.uni-pr.edu>
2023-03-07 01:04:50 +0100

    pjesa e tabit decrypt: id jon ndrru algoritmi per decrypt

--- encode_decode.js
+++ encode_decode.js
@@ -35,6 +35,35 @@ function encrypt(){
     document.getElementById('output').value = output
 }
 
+function encrypt1(){
+    if(!validate1())
+        return;
+    let a = parseInt(document.getElementById('a-key-dec').value)
+    let b = parseInt(document.getElementById('b-key-dec').value)
+
+    let inversi=1
+        for(let i = 1; i<26; i++)
+          if((a*i)%26==1)
+          inversi=i
+
+
+    let input = document.getElementById('input-dec').value
+    input = input.toLowerCase()
+
+    let output = ''
+    for(let i = 0; i< input.length; i++){
+        let p = input.charAt(i)
+        if(p === '')
+            output += p;
+        else {
+            console.log(input.charCodeAt(i)-97, (a * (input.charCodeAt(i) - 97) + b) )
+            output += String.fromCharCode(((input.charCodeAt(i) - 97) - b + 26) * inversi % 26 + 97)
+        }
+    }
+
+    document.getElementById('output-dec').value = output
+}
+
 function validate(){
     // TODO: validate decimal numbers and a has multiplicative inverse mod26, and b<=25
     //  let a_key_possibilities = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25];
@@ -55,6 +84,33 @@ function validate(){
         document.getElementById('invalid-keys-message').style.display = 'block'
     else {
         document.getElementById('invalid-keys-message').style.display = 'none'
+        a.style.borderColor = '#aafcb8'
+        b.style.borderColor = '#aafcb8'
+    }
+
+    return res;
+}
+
+function validate1(){
+    // TODO: validate decimal numbers and a has multiplicative inverse mod26, and b<=25
+    //  let a_key_possibilities = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25];
+    //     a_key_possibilities.find(k => k === a)
+
+    let a = document.getElementById('a-key-dec')
+    let b = document.getElementById('b-key-dec')
+    let res = true
+    if(!a.value || isNaN(a.value)){
+        a.style.borderColor = 'red'
+        res = false
+    }
+    if(!b.value || isNaN(b.value)){
+        b.style.borderColor = 'red'
+        res = false
+    }
+    if(!res)
+        document.getElementById('invalid-keys-message-dec').style.display = 'block'
+    else {
+        document.getElementById('invalid-keys-message-dec').style.display = 'none'
         a.style.borderColor = '#aafcb8'
         b.style.borderColor = '#aafcb8'
     }
