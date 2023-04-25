8ddb87c0c29b977a52152a7af160d888bcff4be2
Rinesa Morina <rinesam8@gmail.com>
2023-04-26 01:36:18 +0200

    added aes-gcm-decryption

--- AES_GCM/GCM.java
+++ AES_GCM/GCM.java
@@ -3,16 +3,20 @@ package AES_GCM;
 public class GCM {
     private AES AES_Algorithm;
     private String plaintext; //input
+    private String ciphertext;
     private String VI;
     private String counter;
     private String generalCounter; // VI + COUNTER
+    private String generalCounterDecryption;
     private String[] BlocksGCM = new String[4]; //supposing qe plaintext i ka 4X16 bit
+    private String[] BlocksGCMDecrypted = new String[4];
 
     public GCM(AES AES_Algorithm) {
         this.AES_Algorithm = AES_Algorithm;
         VI = Utilities.generateIV(10);
         counter = "0000";
         generalCounter = VI + counter;
+        generalCounterDecryption = VI + counter;
     }
     public String[] encryptBlocksGCM(String plaintext){
         this.plaintext = plaintext;
@@ -53,6 +57,25 @@ public class GCM {
         return  result.toString();
     }
     public String[] decryptBlocksGCM(String cipherText){
-        return null;
+        this.ciphertext = cipherText;
+        //divide ciphertext into blocks
+        String[] blocks = new String[4];
+        for (int i= 0,j=0; i < 13 && j<4; i+=4, j++){
+            blocks[i] = cipherText.substring(i, i+4);
+        }
+
+        //encrypt each block from the ciphertext
+        int length = blocks.length;
+        for (int i = 0; i < length; i++){
+            //skip on first time because counter starts at 0000
+            if (i !=0 ){
+                generalCounterDecryption = Utilities.increaseBinaryByOne(generalCounterDecryption);
+            }
+            //encrypt GeneralCounter
+            String encryptedGeneralCounterDecryption = AES_Algorithm.encrypt(generalCounterDecryption);
+            //calculate XOR
+            BlocksGCMDecrypted[i] = calculate_CTi(encryptedGeneralCounterDecryption, blocks[i]);
+        }
+        return BlocksGCMDecrypted;
     }
 }
