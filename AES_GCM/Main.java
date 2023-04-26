c0d68398c2d6cfa287e775f35e5549a05cb05517
Rinesa Morina <rinesam8@gmail.com>
2023-04-26 12:59:08 +0200

    test

--- AES_GCM/Main.java
+++ AES_GCM/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         KeyGenerator keyGenerator = new KeyGenerator("CAF1");
         AES c = new AES(keyGenerator);
-//        String plain_text = "AE24";
+        //String plain_text = "AE24";
         String plain_text = "10101110001001000";
-//        System.out.println("Plain Text:" + plain_text);
-//        String cipher_text = c.encrypt(plain_text);
-//        System.out.println("\nResult = " + cipher_text);
-//        System.out.println("--------------------------------");
-//        String decipher_text = c.decrypt(cipher_text);
-//        String result = Utilities.getMessageFromBinary(decipher_text);
-//        System.out.println("\nResult = " + result);
+        System.out.println("Plain Text:" + plain_text);
+        String cipher_text = c.encrypt(plain_text);
+        System.out.println("\nResult = " + cipher_text);
+        System.out.println("--------------------------------");
+        String decipher_text = c.decrypt(cipher_text);
+        String result = Utilities.getMessageFromBinary(decipher_text);
+        System.out.println("\nResult = " + result);
 
-        GCM g = new GCM(c, "0000000000000000");
+//        GCM g = new GCM(c, "0000000000000000");
         //g.encryptBlocksGCM("10101110001001000");
 //        System.out.println(c.encrypt(plain_text));
 
